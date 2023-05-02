376e19dcab343244d1710f0cd6a2e0a9a85389a8
marcygon <marciasofia49@gmail.com>
2023-05-02 12:54:16 +0200

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,9 +3,16 @@ import './Navbar.scss'
 
 function Navbar() {
   return (
-    <div>
-      <p>hi</p>
-    </div>
+    <nav id='navbar'>
+      <div className='left'>
+        <li className='navbarLi'>EVENTS</li>
+        <li className='navbarLi'>ABOUT</li>
+        <li className='navbarLi'>OUR STORY</li>
+      </div>
+      <div className='right'>
+        <li className='navbarLi'>ACCOUNT</li>
+      </div>
+    </nav>
   )
 }
 
