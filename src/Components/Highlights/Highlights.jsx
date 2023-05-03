aa8e1188c25954e4e6430f8ff76b2229e7aaf5d1
marcygon <marciasofia49@gmail.com>
2023-05-03 22:29:56 +0200

    mainPageSlogan

---
+++ src/Components/Highlights/Highlights.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Highlights.scss'
+
+function Highlights() {
+  return (
+    <div>
+      hola
+    </div>
+  )
+}
+
+export default Highlights
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Banner from '../Components/Banner/Banner'
+import Slogan from '../Components/Slogan/Slogan'
+import Highlights from '../Components/Highlights/Highlights'
 
 
 function MainPage() {
@@ -8,6 +10,8 @@ function MainPage() {
     <>
       <Navbar/>
       <Banner/>
+      <Slogan/>
+      <Highlights/>
     </>
   )
 }
