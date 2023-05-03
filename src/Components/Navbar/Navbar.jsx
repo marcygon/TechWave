6ab0b4e3ccac8e21f8863f16de4e86c453dfabbe
marcygon <marciasofia49@gmail.com>
2023-05-03 18:35:20 +0200

    responsiveMenu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import './Navbar.scss'
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { AiFillFire, AiOutlineQuestion, AiOutlineBook, AiOutlineUser } from "react-icons/ai";
+
 
 function Navbar() {
   return (
+    <>
+    <RxHamburgerMenu id='burguerIcon'/>
     <nav id='navbar'>
+      <RxCross1 id='crossIcon'/>
       <div className='left'>
-        <li className='navbarLi'>EVENTS</li>
-        <li className='navbarLi'>ABOUT</li>
-        <li className='navbarLi'>OUR STORY</li>
+        <li className='navbarLi'><AiFillFire className='navbarIcons'/>EVENTS</li>
+        <li className='navbarLi'><AiOutlineQuestion className='navbarIcons'/>ABOUT</li>
+        <li className='navbarLi'><AiOutlineBook className='navbarIcons'/>OUR STORY</li>
       </div>
       <div className='right'>
-        <li className='navbarLi'>ACCOUNT</li>
+        <li className='navbarLi'><AiOutlineUser className='navbarIcons'/>ACCOUNT</li>
       </div>
     </nav>
+    </>
   )
 }
 
