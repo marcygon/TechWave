633cca88fb03cee7ecf42efd2fd5c6b0e3fb45e5
marcygon <marciasofia49@gmail.com>
2023-07-03 17:06:41 +0200

    deleted everything for a fresh start

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,23 +1,9 @@
 import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
-import Banner from '../Components/Banner/Banner'
-import Slogan from '../Components/Slogan/Slogan'
-import Highlights from '../Components/Highlights/Highlights'
-import StayTunned from '../Components/StayTunned/StayTunned'
-import About from '../Components/About/About'
-import Footer from '../Components/Footer/Footer'
-
 
 function MainPage() {
   return (
     <>
-      <Navbar/>
-      <Banner/>
-      <Slogan/>
-      <Highlights/>
-      <StayTunned/>
-      <About/>
-      <Footer/>
+      <p>hi</p>
     </>
   )
 }
