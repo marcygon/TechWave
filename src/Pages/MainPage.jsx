575cb3f862792357ab59ff700e36d86e919ea64d
marcygon <marciasofia49@gmail.com>
2023-07-04 12:42:07 +0200

    slogan

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import Slogan from '../Components/Slogan/Slogan'
 
 function MainPage() {
   return (
     <>
     <Navbar/>
+    <Slogan/>
     </>
   )
 }
