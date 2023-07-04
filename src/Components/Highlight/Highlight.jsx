046d12a1a2acab2eead528b4d3537f6d124bd05b
marcygon <marciasofia49@gmail.com>
2023-07-04 16:27:17 +0200

    highlights added

--- src/Components/Highlight/Highlight.jsx
+++ src/Components/Highlight/Highlight.jsx
@@ -1,36 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import pic from '../../Assets/girls-group.jpg'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { CardActionArea } from '@mui/material';
 
+import TechWaveServices from '../../Services/TechWaveService'
 
 function Highlight() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={pic}
-        title="girls group"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  )
+
+    const [highLights, setHighLights] = useState([])
+
+    useEffect(() => {
+        TechWaveServices.findHighlights()
+            .then((data) => {
+                setHighLights(data)
+            })
+    }, []);
+
+    return (
+
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container  spacing={2}>
+                {highLights.map((item) => (
+                    <Grid item xs={12} sm={6} md={4} key={item.id} container justifyContent="center">
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image={item.img}
+                                    title={item.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {item.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {item.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
 }
 
 export default Highlight
