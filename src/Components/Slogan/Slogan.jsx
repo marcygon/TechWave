a187906ea411c9265ba4b21f5d0ec46751fda034
marcygon <marciasofia49@gmail.com>
2023-07-05 13:47:28 +0200

    slogan linked button

--- src/Components/Slogan/Slogan.jsx
+++ src/Components/Slogan/Slogan.jsx
@@ -3,6 +3,7 @@ import './Slogan.scss'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom'
 
 function Slogan() {
 
@@ -12,7 +13,9 @@ function Slogan() {
                 <Grid item xs={12}>
                     <div className='slogan-text'>
                         <h1>Don't miss a thing, join our community now!</h1>
-                        <Button variant="outlined" href='#'>See events</Button>
+                        <Link to={`/events`} style={{ textDecoration: 'none' }}>
+                            <Button variant="outlined">See events</Button>
+                        </Link>
                     </div>
                 </Grid>
             </Grid>
