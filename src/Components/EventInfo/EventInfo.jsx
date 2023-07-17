086b2c4890466b59e308ac9588beabe91d4d2e9e
marcygon <marciasofia49@gmail.com>
2023-07-17 18:16:48 +0200

    join an event increase the register count and shows a succes message

--- src/Components/EventInfo/EventInfo.jsx
+++ src/Components/EventInfo/EventInfo.jsx
@@ -15,89 +15,113 @@ import TechWaveServices from '../../Services/TechWaveService'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
-import Collapse from '@mui/material/Collapse';
+import Stack from '@mui/material/Stack';
 
 
 function EventInfo() {
 
-
     const [eventId, setEventId] = useState([])
     const [showAlert, setShowAlert] = useState(false);
     const { id } = useParams()
+
     useEffect(() => {
         TechWaveServices.eventById(id)
             .then((data) => setEventId(data))
     }, [id])
 
     const join = () => {
-        setShowAlert(true);
+
+        setEventId((prevState) => ({
+            ...prevState,
+            registersCount: prevState.registersCount + 1
+        }));
+
+        setShowAlert(true)
+
         setTimeout(() => {
             setShowAlert(false);
-        }, 3000);
-    };
+        }, 3000);;
+    }
 
 
     return (
-        <Box sx={{
-            flexGrow: 1,
-            marginTop: 2,
-            marginBottom: 5,
-        }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={6} md={4} key={eventId.id} container justifyContent="center">
-                    <Card sx={{ maxWidth: 345 }}>
-                        <CardActionArea>
-                            <CardMedia
-                                sx={{ height: 140 }}
-                                image={eventId.img}
-                                title={eventId.name}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {eventId.name}
-                                </Typography>
-                                <Typography variant="h6">
-                                    {eventId.organizer}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {eventId.eventDate} {eventId.eventHour}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary" gutterBottom="true">
-                                    {eventId.location}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {eventId.description}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                            </IconButton>
-                            <IconButton aria-label="share">
-                                <ShareIcon />
-                            </IconButton>
-                            <Box sx={{ marginLeft: 'auto' }}>
-                                <Button
-                                    className="joinBtn"
-                                    variant="contained"
-                                    aria-label="join"
-                                    startIcon={<AddCircleOutlineIcon />}
-                                    onClick={join}
-                                >
-                                    Join {eventId.registersCount}/{eventId.maxParticipants}
-                                </Button>
-                            </Box>
-                        </CardActions>
-                    </Card>
+        <>
+            <Box sx={{
+                flexGrow: 1,
+                marginTop: 2,
+                marginBottom: 5,
+            }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6} md={4} key={eventId.id} container justifyContent="center">
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image={eventId.img}
+                                    title={eventId.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {eventId.name}
+                                    </Typography>
+                                    <Typography variant="h6">
+                                        {eventId.organizer}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {eventId.eventDate} {eventId.eventHour}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" gutterBottom="true">
+                                        {eventId.location}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {eventId.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton>
+                                <Box sx={{ marginLeft: 'auto' }}>
+                                    <Button
+                                        className="joinBtn"
+                                        variant="contained"
+                                        aria-label="join"
+                                        startIcon={<AddCircleOutlineIcon />}
+                                        onClick={join}
+                                    >
+                                        Join {eventId.registersCount}/{eventId.maxParticipants}
+                                    </Button>
+                                </Box>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Collapse in={showAlert} timeout={1000}>
-                <Alert severity="success" sx={{ mt: 2 }}>
-                    Event joined
-                </Alert>
-            </Collapse>
-        </Box>
+            </Box>
+
+            {/* <Alert severity="error">Event declened</Alert>*/}
+            <Stack
+                sx={{
+                    width: '100%',
+                    position: 'fixed',
+                    bottom: 16,    
+                }}
+                spacing={2}
+            >
+                {showAlert && (
+                    <Alert
+                        severity="success"
+                        onClose={() => setShowAlert(false)}
+                        sx={{ width: '100%' }}
+                    >
+                        Event joined!
+                    </Alert>
+                )}
+            </Stack>
+        </>
     )
 }
 
