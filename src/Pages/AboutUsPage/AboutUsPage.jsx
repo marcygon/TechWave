b5eac832b3c20ef1e6771c1a600f13e7f6cf7949
marcygon <marciasofia49@gmail.com>
2023-07-19 14:06:41 +0200

    image list

--- src/Pages/AboutUsPage/AboutUsPage.jsx
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
 import AboutUs from '../../Components/AboutUs/AboutUs';
+import QuiltedImageList from '../../Components/ImageList/ImageList';
 
 
 function AboutUsPage() {
@@ -9,6 +10,7 @@ function AboutUsPage() {
         <>
             <Navbar />
             <AboutUs />
+            <QuiltedImageList />
             <Footer />
         </>
     )
