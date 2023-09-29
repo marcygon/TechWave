5e322a98780830df1d08c9d83448b7434153d8bd
marcygon <marciasofia49@gmail.com>
2023-09-30 00:08:23 +0200

    aboutUs centered

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import './AboutUs.scss'
 
 function AboutUs() {
   return (
-    <Box sx={{
-      margin: '50px 30px 50px 30px',
-    }}>
+    <Box id='aboutUsBox'>
       <Typography variant="body1" color="text.secondary" align='justify'>
         Techwave is a platform that focuses on organizing and promoting technology-related events. As user you can esaily view available events and register for those that you're interested in. Ride the wave of innovation with Techwave - your ultimate destination for technology events.
       </Typography>
     </Box>
-
   )
 }
 
