968795812dd4591e3443b077e1a390f49223f0f5
marcygon <marciasofia49@gmail.com>
2023-10-02 19:30:01 +0200

    require auth component for user and admin

---
+++ src/Components/RequireAuth/RequireAuth.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const RequireAuth = ({ children, redirectTo = "/login" }) => {
+
+    const userItem = JSON.parse(localStorage.getItem("auth"));
+
+    if (!userItem) {
+        return <Navigate to={redirectTo} />
+    }
+    return children ? children : <Outlet />
+};
+
+export default RequireAuth    
