2015370d3d03d70d46b20188e4812985712e54d5
marcygon <marciasofia49@gmail.com>
2023-10-06 12:07:49 +0200

    name valitaion for register

--- src/Components/Validations/RegisterValidation.jsx
+++ src/Components/Validations/RegisterValidation.jsx
@@ -1,8 +1,9 @@
 function registerValidation() {
     var email = document.getElementById('email')
     var password = document.getElementById('password')
-    if (email === "" || password === "") {
-      alert("Email and password must be filled out.");
+    var name = document.getElementById('name')
+    if (email === "" || password === "" || name === "") {
+      alert("Name, email and password must be filled out.");
       return false;
     }}
 
