d050191205d8cb3d1a3e1e3aabe5d13db705101d
marcygon <marciasofia49@gmail.com>
2023-11-20 18:12:56 +0100

    slogan update

--- src/Components/Slogan/Slogan.jsx
+++ src/Components/Slogan/Slogan.jsx
@@ -1,26 +1,54 @@
 import React from 'react'
-import './Slogan.scss'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom'
+import AboutUs from '../AboutUs/AboutUs';
 
 function Slogan() {
 
     return (
-        <Box sx={{ flexGrow: 1 }} className='slogan'>
-            <Grid container spacing={2}>
+        <Box
+            sx={{
+                display: 'flex',
+                margin: { xs: '30px 13px', md: '70px 60px' },
+            }}
+        >
+            <Grid
+                container
+                spacing={2}
+            >
                 <Grid
-                    item xs={12}
+                    item
+                    xs={12}
+                    md={6}
                     sx={{
-                        marginTop: '30px',
-                    }}>
-                    <div className='slogan-text'>
-                        <h1>Don't miss a thing, join our community now!</h1>
-                        <Link to={`/events`} style={{ textDecoration: 'none' }}>
-                            <Button variant="outlined">See events</Button>
-                        </Link>
-                    </div>
+                        textAlign: 'center',
+                    }}
+                >
+                    <h1>Don't miss a thing, join our community now!</h1>
+                    <Link
+                        to={`/events`}
+                        style={{
+                            textDecoration: 'none'
+                        }}
+                    >
+                        <Button
+                            variant="outlined"
+                            sx={{
+                                marginTop: '15px',
+                            }}
+                        >
+                            See events
+                        </Button>
+                    </Link>
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    md={6}
+                >
+                    <AboutUs />
                 </Grid>
             </Grid>
         </Box>
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -7,7 +7,6 @@ function MainPage() {
   return (
     <>
       <Slogan />
-      <AboutUs />
       <Highlight />
     </>
   )
