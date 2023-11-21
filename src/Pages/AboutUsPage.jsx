e8e06ef51220eb9a0d84e6a010b7bfbef1e1a171
marcygon <marciasofia49@gmail.com>
2023-11-21 21:57:55 +0100

    modified about us page

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,63 +1,40 @@
 import React from 'react'
 import AboutUs from '../Components/AboutUs/AboutUs';
 import EventsImageList from '../Components/EventsImageList/EventsImageList';
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 function AboutUsPage() {
     return (
-        <Grid
-            container
-            spacing={2}
-        >
-            <Grid
-                item
-                md={3}
+        <>
+            <Box
+                sx={{
+                    padding: '20px',
+                    margin: '30px 0px'
+                }}
             >
-                <Box
+                <Typography
+                    variant='h4'
                     sx={{
-                        backgroundColor: '#6495ED',
-                        padding: '20px',
-                        writingMode: 'vertical-rl', 
+                        marginBottom: '10px'
                     }}
                 >
-                    <Typography
-                        variant='h4'
-                        textAlign='center'
-                        >
-                        About us
-                    </Typography>
-                </Box>
-            </Grid>
-            <Grid
-                item
-                xs={12}
-                md={6}
-            >
-                <Box
+                    About us.
+                </Typography>
+                <AboutUs />
+                <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    align='justify'
                     sx={{
-                        padding: '20px',
-                        height: '100%'
+                        marginTop: '10px'
                     }}
                 >
-                    <AboutUs />
-                </Box>
-            </Grid>
-            <Grid
-                item
-                md={3}
-            >
-                <Box
-                    sx={{
-                        backgroundColor: '#ADD8E6',
-                        height: '100%'
-                    }}
-                >
-                </Box>
-            </Grid>
-            <Grid>
-                <EventsImageList />
-            </Grid>
-        </Grid>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aut facilis nesciunt aliquid laborum porro sint veniam mollitia sapiente, debitis iste a deleniti voluptatum harum inventore velit? Hic sed perferendis, corporis vel accusantiu illum corporis odio nostrum, possimus earum aperiam nobis est nemo ipsum fugiat. A, commodi mollitia!
+                </Typography>
+            </Box>
+
+            <EventsImageList />
+        </>
     )
 }
 
