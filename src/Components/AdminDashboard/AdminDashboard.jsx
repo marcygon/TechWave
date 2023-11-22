9fa85a43117861c6cbd7d921d68079df96c6ad47
marcygon <marciasofia49@gmail.com>
2023-11-22 20:53:11 +0100

    admin dashboard edited to a list with delet and edit buttons non functional

--- src/Components/AdminDashboard/AdminDashboard.jsx
+++ src/Components/AdminDashboard/AdminDashboard.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import AllEvents from '../Events/AllEvents'
+import React, { useEffect, useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import TechWaveServices from '../../Services/TechWaveService';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 function AdminDashboard() {
+
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    TechWaveServices.getAllEvents()
+      .then((data) => {
+        setEvents(data)
+      })
+  }, []);
+
+  const handleDelete = (eventId) => {
+    // Add the logic to delete the event with the specified eventId
+    // You might want to show a confirmation dialog before deleting
+    console.log(`Delete event with ID ${eventId}`);
+  };
+
   return (
-    <>
-      <AllEvents />
-    </>
+    <List
+      sx={{
+        width: '100%',
+      }}
+    >
+      {events.map((i) => {
+        return (
+          <ListItem
+            key={i}
+            secondaryAction={
+              <>
+                <IconButton
+                  aria-label="edit"
+                >
+                  <ModeEditIcon />
+                </IconButton>
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDelete(i.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </>
+            }
+            disablePadding
+          >
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar
+                  alt={i.name}
+                  src={i.img}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                id={i.id}
+                primary={i.name}
+              />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
   )
 }
 
